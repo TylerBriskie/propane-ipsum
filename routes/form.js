a5cf2f9a806f9890a2d92b5413db8f6cc54abfa9
Tyler Briskie <tylerbriskie@gmail.com>
2019-08-04 14:31:04 -0600

    add form for adding a quote

--- routes/form.js
+++ routes/form.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/form', function(req, res, next) {
-    console.log('hi')
-    res.render('index', {
-        title: 'Strickland Ipsum',
-        subtitle: 'Placeholder text and placeholder text accessories since 2019'
+router.get('/', function(req, res, next) {
+    res.render('form', {
+        title: 'Add a quote',
     });
 });
 
