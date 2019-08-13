9598f031a24be877661d4f622d5db282993bfea3
Tyler Briskie <tylerbriskie@gmail.com>
2019-08-12 19:02:44 -0600

    switching computers

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const DBCONN = require('./config/keys');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var formRouter = require('./routes/form');
