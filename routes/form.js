489cc373ba31ce71a717bc71574dbc1b9b17367b
Tyler Briskie <tylerbriskie@gmail.com>
2019-08-13 18:51:30 -0600

    Mongo DB integrated :sparkles:

--- routes/form.js
+++ routes/form.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log("cookie? : ", req.cookies);
+    jwt.verify(req.cookies.token, 'DangOlYoManTalkinBoutTryinaHackMyMainframe', (err, decoded) => {
+        if (decoded){
+            console.log('welcome t-bone');
+        } else {
+            res.redirect('/login');
+        }
+    })
+
     res.render('form', {
         title: 'Add a quote',
     });
