a9be6323fae4edcca8e83eb5a92f8759addcb8eb
Tyler Briskie <tylerbriskie@gmail.com>
2019-08-15 21:45:17 -0600

    adding reload button

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,44 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
+const Quote = require('../models/Quote');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('lets see some quotes!');
-  res.render('index', {
-    title: 'Strickland Ipsum',
-    subtitle: 'Placeholder text and placeholder text accessories since 2019'
+  // GET quotes
+  let formattedHeadings = [];
+  let formattedSubheadings = [];
+  let formattedQuotes = [];
+
+  Quote.aggregate([{$sample: { size: 40 }}]).then((quotes)=> {
+    for (let i = 0; i<quotes.length; i++){
+      switch (quotes[i].length){
+        case "LONG":
+          formattedQuotes.push(quotes[i].quote);
+
+          break;
+        case "MEDIUM":
+          formattedSubheadings.push(quotes[i].quote);
+          console.log("med boy: ", quotes[i].quote);
+          break;
+        case "SHORT":
+          formattedHeadings.push(quotes[i].quote);
+          break;
+        default:
+          formattedQuotes.push(quotes[i].quote);
+      }
+    }
+    console.log('FORMATTED QUOTES: ', formattedQuotes);
+    res.render('index', {
+      title: 'Strickland Ipsum',
+      subtitle: 'Placeholder text and placeholder text accessories since 2019',
+      headings: formattedHeadings,
+      subHeadings: formattedSubheadings,
+      quotes: formattedQuotes,
+    });
   });
+
+
+
 });
 
 module.exports = router;
